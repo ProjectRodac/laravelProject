758bdcc4f888bc228fa843e0686a069f5a5b8408
ProjectRodac <nikola.rodes@hotmail.com>
2016-05-22 22:34:14 +0200

    CSS
    Custom css
    <link rel="stylesheet" href="{{ URL::to('/css/bootstrap.min.css') }}">
    <link rel="stylesheet" href="{{ URL::to('/css/main.css') }}">

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,44 @@
 @extends('layouts.master')
 
-@section('title')
-    Welcome!
-@endsection
-
 @section('content')
-    <h1>The Dashboard</h1>
+    <section class="row new-post">
+        <div class="col-md-10 col-md-offset-1">
+            <header><h3>Ažuriraj status</h3></header>
+            <form action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Što vam je na umu?"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Objavi</button>
+            </form>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-10 col-md-offset-1">
+            <header><h3>Livefed</h3></header>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias, aliquam animi asperiores at cupiditate distinctio ea eligendi excepturi ipsa labore, neque nulla odio porro praesentium suscipit veniam voluptas voluptatibus.</p>
+                <div class="info">
+                    Objavio Rodac, 22.05.2016.
+                </div>
+                <div class="interaction">
+                    <a class="glyphicon glyphicon-thumbs-up" href="#"></a>
+                    <a class="glyphicon glyphicon-thumbs-down" href="#"></a>
+                    <a class="glyphicon glyphicon-pencil" href="#"></a>
+                    <a class="glyphicon glyphicon-remove" href="#"></a>
+                </div>
+            </article>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias, aliquam animi asperiores at cupiditate distinctio ea eligendi excepturi ipsa labore, neque nulla odio porro praesentium suscipit veniam voluptas voluptatibus.</p>
+                <div class="info">
+                    Objavio Rodac, 22.05.2016.
+                </div>
+                <div class="interaction">
+                    <a class="glyphicon glyphicon-thumbs-up" href="#"></a>
+                    <a class="glyphicon glyphicon-thumbs-down" href="#"></a>
+                    <a class="glyphicon glyphicon-pencil" href="#"></a>
+                    <a class="glyphicon glyphicon-remove" href="#"></a>
+                </div>
+            </article>
+        </div>
+    </section>
 @endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,25 +1,24 @@
 <header>
-  <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="{{ route('dashboard') }}">Brand</a>
-      </div>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ route('dashboard') }}">Rodac's Social Network</a>
+            </div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        @if (!Auth::guest())
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="{{ route('logout') }}">Logout</a></li>
-        </ul>
-          @endif
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="mainNavBar">
+                @if (!Auth::Guest())
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ route('logout') }}">Logout</a></li>
+                </ul>
+                @endif
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 </header>
