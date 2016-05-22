860dbb5cd1a58603c4ec27bc9494d11f2fb58aa5
ProjectRodac <nikola.rodes@hotmail.com>
2016-05-22 11:41:35 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome!
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+        <h3>Sign Up</h3>
+            <form action="{{ route('signup') }}" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                    <label for="first_name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
+            </form>
+        </div>
+        <div class="col-md-6">
+        <h3>Sign In</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>                    
+    </div>
+@endsection
